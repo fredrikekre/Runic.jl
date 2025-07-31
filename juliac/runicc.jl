a5084272126cdbc4a15742dac6eb7734eb16e151
Romeo Valentin <romeov@stanford.edu>
2025-07-31 16:45:37 -0700

    Add `@main` to Runic module
    This now supports the `-m` and the `juliac` workflow.

--- juliac/runicc.jl
+++ juliac/runicc.jl
@@ -1,7 +1,7 @@
 using Runic: Runic
 
 # Compileable main function corresponding to `int main(int argc, char** argv)`
-function @main(args::Vector{String})::Cint
+function (@main)(args::Vector{String})::Cint
     if length(args) == 1
         Runic.print_help()
         return 0
